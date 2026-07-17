7f7a5443e9a759655ba5f1581b9a62f2278403a0
pavelbekarev <bekarev.ps@gmail.com>
2026-07-17 15:13:13 +0500

    feat: design project architecture

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,21 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import { defineConfig } from "vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
-})
+  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  resolve: {
+    alias: {
+      "#shared": path.resolve(__dirname, "src/shared"),
+      "#widgets": path.resolve(__dirname, "src/widgets"),
+      "#features": path.resolve(__dirname, "src/features"),
+      "#entities": path.resolve(__dirname, "src/entities"),
+      "#pages": path.resolve(__dirname, "src/pages"),
+      "#app": path.resolve(__dirname, "src/app"),
+      "#assets": path.resolve(__dirname, "public/assets"),
+      "#fonts": path.resolve(__dirname, "public/fonts"),
+    },
+  },
+});
