7e072818dc8d4a652c8a35d4cdc542047d3d1d35
pavelbekarev <bekarev.ps@gmail.com>
2026-07-17 17:49:35 +0500

    fix: fix warnings in console

--- src/entities/Training/ui/TrainerHeader.tsx
+++ src/entities/Training/ui/TrainerHeader.tsx
@@ -1,11 +1,10 @@
 import type { Trainer } from "#entities/Trainer";
-import avatar from "../../../../public/avatar.webp"
 import "../style.scss"
 
 export function TrainerHeader({ trainer, station }: {trainer: Trainer, station: string}) {
     return (
         <div className="trainerHeader">
-            <img className="trainerHeader__avatar" src={avatar} alt="Аватар" />
+            <img className="trainerHeader__avatar" src="/avatar.webp" alt="Аватар" />
             <div className="trainerHeader__text">
                 <h2 className="trainerHeader__trainerName">{ trainer.fullName }</h2>
                 <span className="trainerHeader__station">{station}</span>
